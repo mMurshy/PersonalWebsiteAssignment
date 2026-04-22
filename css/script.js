9b2efd87163e0c29baacb54a21f9c3a7ca7e2720
mMurshy <jackmbgrant@gmail.com>
2026-04-22 15:05:41 -0400

    4/22 changes broken drawing code
    code written, script not functional

---
+++ css/script.js
@@ -0,0 +1,53 @@
+const canvas = document.querySelector("canvas"),
+sizeSlider = document.querySelector("#size-slider"),
+colorBtns = document.querySelectorAll(".colors .option"),
+colorPicker = document.querySelector("#color-picker"),
+clearCanvas = document.querySelector("#clear-canvas")
+ctx = canvas.getContext("2d"),
+
+isDrawing = false,
+selectedTool = "brush",
+brushWidth = 5,
+selectedColor = "#000";
+
+window.addEventListener("load", () => {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+});
+
+const startDraw = () => {
+    isDrawing = true
+    ctx.beginPath();
+    ctx.lineWidth = brushWidth;
+    ctx.strokeStyle = selectedColor;
+}
+
+const drawing = (e) => {
+    if(!isDrawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+    document.querySelector("options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+    selectedColor = window.getComputedStyle(btn).getPropertyPriority("background-color");
+    });
+});
+
+colorPicker.addEventListener("change", () => {
+    colorPicker.parentElement.style.background = colorPicker.value
+    colorPicker.parentElement.click();
+})
+
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRec(0,0, canvas.width, canvas.height);
+})
+
+canvas.addEventListener("mousedown", startDraw);
+canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
